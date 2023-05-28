662c3f1cf2f6ce83e42a8adab3951809926262ac
Smith Thongbai <thonsm06@gmail.com>
2023-05-28 09:16:45 -0500

    Added some operator buttons' logics

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # calculator-v2
+
+BUTTONS BEHAVIVOR:
+-operator buttons
+    when pressing one of the operators button
+    if num1/operate/num2
+        then calculate the current equation
+        and set result to num 1 and set new operator
+        keep display1 to num2 
+        update display2 to num1 with the new operator
+            *if pressed operator again and again, keep num2 the same 
+            *but change num1 to new result calculated with the pressed operator
+    else if num1/operate and not num2
+        set new operator,
+        update display with new operator
+            *should only update the operator on display and internal
+    else if num1
+        then set operator 
+        push to display1
+        clear display2
--- script.js
+++ script.js
@@ -11,37 +11,57 @@ function divide(a, b) {
     return a / b;
 }
 
-let number1 = 0;
-let number2 = 0;
+let number1 = '';
+let number2 = '';
 let operator = '';
-
+let result = '';
 //runs when user hit = or enter
 function operate(operator, num1, num2) {
     add(num1, num2);
 }
 
-const display = document.querySelector('#lower');
+const display1 = document.querySelector('#upper');
+const display2 = document.querySelector('#lower');
 
 const digitButtons = document.querySelectorAll('.digit');
 digitButtons.forEach(button => button.addEventListener('click', e => {
+
+    number1 += e.target.textContent;
     updateDisplay(e.target.textContent); //add the digit directly to display
 }))
 
 const operatorButtons = document.querySelectorAll('.operator');
 operatorButtons.forEach(button => button.addEventListener('click', e => {
-    //
-    updateDisplay(e.target.textContent);
+
+    if (number1 !== '' && operator !== '' && number2 !== ''){
+        result = operate(operator, number1, number2);
+        number1 = result;
+        operator = e.target.textContent;
+        display2.textContent = number1 + operator;
+    } else if (number1 !== '' && operator !== '' && number2 === '') {
+        operator = e.target.textContent;
+        display1.textContent = number1 + operator;
+    } else if (number1 !== '' && operator === '' && number2 === '') {
+        operator = e.target.textContent;
+        display1.textContent = number1 + operator;
+        display2.textContent = '';
+    }
+
+    //updateDisplay(e.target.textContent);
 }))
 
 function updateDisplay(num1) {
-    display.textContent += num1;
+    display2.textContent += num1;
 };
 
 
 
 const clearButton = document.querySelector('#clear');
 clearButton.addEventListener('click', clearScreen);
-
 function clearScreen() {
-    display.textContent = '';
+    display1.textContent = '';
+    display2.textContent = '';
+    number1 = '';
+    number2 = '';
+    operator = '';
 };
