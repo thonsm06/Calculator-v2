f8247b22802896bd06bdf6786e8603035785e2da
Smith Thongbai <thonsm06@gmail.com>
2023-05-28 08:34:27 -0500

    Added clear screen function

--- script.js
+++ script.js
@@ -19,3 +19,29 @@ let operator = '';
 function operate(operator, num1, num2) {
     add(num1, num2);
 }
+
+const display = document.querySelector('#lower');
+
+const digitButtons = document.querySelectorAll('.digit');
+digitButtons.forEach(button => button.addEventListener('click', e => {
+    updateDisplay(e.target.textContent); //add the digit directly to display
+}))
+
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach(button => button.addEventListener('click', e => {
+    //
+    updateDisplay(e.target.textContent);
+}))
+
+function updateDisplay(num1) {
+    display.textContent += num1;
+};
+
+
+
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', clearScreen);
+
+function clearScreen() {
+    display.textContent = '';
+};
