bd4f9b54b3079a33e75f86e18a4a764c0b7380aa
Smith Thongbai <thonsm06@gmail.com>
2023-05-29 00:35:49 -0500

    Create a function to clear global variables after pressing certain buttons.

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function operate(operator, num1, num2) {
 let number1 = '';    //hold a string of number
 let number2 = '';    //hold a string of number
 let operator = '';   //hold a string of an operator
-//let result = '';     //hold a string of result
+let result = '';     //hold a string of result, can be use for recall
 let lastButton = ''; //hold a string of the last button pressed
 const display1 = document.getElementById('upper');
 const display2 = document.getElementById('lower');
@@ -37,10 +37,10 @@ const display2 = document.getElementById('lower');
 //-----digit buttons-----//////////////////////////////
 const digitButtons = document.querySelectorAll('.digit');
 digitButtons.forEach(button => button.addEventListener('click', event => {
-    addNumber(event, operator, number1, number2, lastButton);
+    setDecider(event, operator, number1, lastButton); //decide which sets the number should go into.
 }));
 
-function addNumber(e, op, num1, num2, lastBtn) {
+function setDecider(e, op, num1, lastBtn) {
     const val = e.target.textContent; //assign content to a local variable
     if (lastBtn === '') {
         if (num1 !== '' && op !== '') {
@@ -73,19 +73,28 @@ function updateDisplay(dis, val) {
     updateDisplay(e.target.textContent); //add the digit directly to display
 })) */
 
+//-----equal button-----//////////////////////////////
 const equalButton = document.getElementById('=');
 equalButton.addEventListener('click', e => {
     const equation = `${number1} ${operator} ${number2}`; //num1/op/num2
     result = operate(operator, number1, number2);   //
     updateDisplay(display1, equation);   
     updateDisplay(display2, result); 
+
+    clearVar('=');
 });
+
+function clearVar(last){
+    number1 = '';
+    number2 = '';
+    operator = '';
+    lastButton = last;
+}
 
 const operatorButtons = document.querySelectorAll('.operator');
 operatorButtons.forEach(button => button.addEventListener('click', e => {
     if (number1 !== '' && operator !== '' && number2 !== ''){
-        
-        const result = operate(operator, number1, number2);
+        result = operate(operator, number1, number2);
         number1 = result;
         operator = e.target.textContent;
         display1.textContent = number1 + operator;
@@ -110,10 +119,7 @@ clearButton.addEventListener('click', clearScreen);
 function clearScreen() {
     display1.textContent = '';
     display2.textContent = '';
-    number1 = '';
-    number2 = '';
-    operator = '';
-    lastButton = 'C'
+    clearVar('C');
 };
 
 
